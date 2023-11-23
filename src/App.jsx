879dd0d23112ffcaf42ad12c6ff6983518a9c96f
Juh1s <juho.koivumaa@aalto.fi>
2023-11-23 11:36:04 +0200

    Personal Training github-tiedostot pohjustettu.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Personal Training</h1>
+    </>
+  )
+}
+
+export default App
